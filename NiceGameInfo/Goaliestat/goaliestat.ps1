2b1b7201e24da333da6a90f0f27adff098319024
Anders <57982062+kkanders@users.noreply.github.com>
2020-03-13 20:38:00 +0100

    HTML templates for CasparCG

---
+++ NiceGameInfo/Goaliestat/goaliestat.ps1
@@ -0,0 +1,33 @@
+﻿ Remove-Variable -ErrorAction SilentlyContinue -WarningAction SilentlyContinue *
+ 
+ $ScriptDir = Split-Path $script:MyInvocation.MyCommand.Path
+
+ $goalie = (Get-Clipboard).split("	")
+ $rank0 = $goalie[0]
+ $name = $goalie[0]
+ $team = $goalie[0]
+ $rank0 = $goalie[12]
+
+
+
+$body = [ordered]@{
+    f0 = $rank0
+    f1 = $name
+}
+$json = $body | ConvertTo-Json -Compress
+$json = $json.Replace('"','\"').replace('`n','')
+$json
+$command = "CG 1-21 ADD 1 ""NiceGameInfo/Goaliestat/GOALIE"" 1 ""$json"""
+
+
+# CG 1-20 ADD 1 "iceGameInfo/ltblue/LOWER THIRD" 1 "{\"f0\":\"test\",\"f1\":\"value2\",\"f2\":\"value3\"}"\r\n
+
+
+add-type -path "$ScriptDir\CasparCGNETConnector.dll"
+$casparcg = new-object "CasparCGNETConnector.CasparCGConnection"
+$casparcg.connect("$env:COMPUTERNAME",5250)
+$casparcg.sendCommand($command)
+sleep(5)
+$casparcg.sendCommand('CG 1-21 STOP 1')
+$casparcg.close()
+
---
+++ NiceGameInfo/Goaliestat/playerliste/makeplayerlist.ps1
@@ -0,0 +1,34 @@
+﻿Remove-Variable *
+cd "C:\Users\amol\Downloads\CasparCG.Server-2.1.8NRK\CasparCG Server\server\template\NiceGameInfo\Goaliestat\playerliste"
+$split = "	"
+$golie = Import-Csv .\hgoalie.csv -Header nr,player -Delimiter $split
+
+$table,$goalie,$homefinalcut,$line = ""
+
+foreach ($line in $golie) {
+#$line.nr
+#$table += "<tr>`r`n"
+
+$table += "<tr>`r`n"
+    $table += "<td class='nr'>$($line.nr)</td>"
+    $table += "<td class='player'>$($line.player)</td>"    
+    $table += "<tr>`r`n"
+
+ }
+
+
+
+
+$homefinalcut += $goalie
+$homefinalcut += $table
+
+$homefile = Get-Content ..\rooster.html
+$homefile = $homefile.Replace('#change#',$homefinalcut)
+$homefile | Out-File ..\homerooster.html -Force 
+
+
+
+
+
+
+#$table
